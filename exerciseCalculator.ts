1b47b758d7e60672d6b7b6c75f98d03d0ee3c32f
mtyler0606 <mtyler0606@gmail.com>
2025-02-26 15:04:52 -0600

    9.3 Command line

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -7,6 +7,27 @@ interface Result {
     target: number,
     average: number
 }
+interface exerciseArgs {
+    inputs: number[],
+    target: number
+}
+
+const parseArgs = (args: string[]): exerciseArgs => {
+    if (args.length < 4) throw new Error("Not enough arguments");
+    let inputs = []
+    if(isNaN(Number(args[2]))){
+        throw new Error("Provided values were not numbers")
+    }
+    for(let i = 3; i < args.length; i++){
+        if(isNaN(Number(args[i]))){
+            throw new Error("Provided values were not numbers")
+        }
+        else{
+            inputs.push(Number(args[i]))
+        }
+    }
+   return { inputs: inputs, target: Number(args[2])}
+}
 
 const exerciseCalculator = (inputData: number[], target: number) => {
     const average = inputData.reduce((accumulator, current) => accumulator + current, 0) / inputData.length
@@ -23,4 +44,16 @@ const exerciseCalculator = (inputData: number[], target: number) => {
     }    
 }
 
-console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
+//console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
+
+try {
+    const { inputs, target } = parseArgs(process.argv)
+    console.log(exerciseCalculator(inputs, target))
+}
+catch (error: unknown) {
+    let errorMessage = "Something went wrong"
+    if(error instanceof Error){
+        errorMessage += 'Error: ' + error.message
+    }
+    console.log(errorMessage)
+}

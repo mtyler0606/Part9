9242f9d0576e05094795ddae23facdd97a7a7411
mtyler0606 <mtyler0606@gmail.com>
2025-02-27 15:57:10 -0600

    9.7 WebExercises

--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -29,7 +29,7 @@ const parseArgs = (args: string[]): exerciseArgs => {
    return { inputs: inputs, target: Number(args[2])};
 };
 
-const exerciseCalculator = (inputData: number[], target: number): Result => {
+export const exerciseCalculator = (inputData: number[], target: number): Result => {
     const average = inputData.reduce((accumulator, current) => accumulator + current, 0) / inputData.length;
     const rating = (average < 0.5 * target)? 1: (average < target)? 2: 3;
     const ratingDescription = (rating === 1)? "not good" : (rating === 2)? "not bad": "good";
@@ -45,15 +45,16 @@ const exerciseCalculator = (inputData: number[], target: number): Result => {
 };
 
 //console.log(exerciseCalculator([3, 0, 2, 4.5, 0, 3, 1], 2))
-
-try {
-    const { inputs, target } = parseArgs(process.argv);
-    console.log(exerciseCalculator(inputs, target));
-}
-catch (error: unknown) {
-    let errorMessage = "Something went wrong";
-    if(error instanceof Error){
-        errorMessage += 'Error: ' + error.message;
+if(require.main === module){
+    try {
+        const { inputs, target } = parseArgs(process.argv);
+        console.log(exerciseCalculator(inputs, target));
     }
-    console.log(errorMessage);
+    catch (error: unknown) {
+        let errorMessage = "Something went wrong";
+        if(error instanceof Error){
+            errorMessage += 'Error: ' + error.message;
+        }
+        console.log(errorMessage);
+    }
 }
--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { exerciseCalculator } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
     res.send("Hello Full Stack");
@@ -21,6 +23,29 @@ app.get('/bmi', (_req, res) => {
             weight: weight,
             bmi: calculateBmi(height, weight)
         });
+    }
+});
+
+app.post('/exercises', (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { dailyExercises, target } = req.body;
+    if(!dailyExercises || !target){
+        res.status(400).send({
+            error: "parameters missing"
+          });
+    }
+
+    else if (isNaN(Number(target))){
+        res.status(400).send({
+            error: "malformatted parameters"
+          });
+    }
+    else{
+    const result = exerciseCalculator(
+        dailyExercises,
+        Number(target)
+    );
+    res.send({result});
     }
 });
 
